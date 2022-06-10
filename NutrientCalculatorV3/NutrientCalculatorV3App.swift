d2858e7044bebdf083e127db240bc31adaaf63e3
Blan32 <kblandford@live.com>
2022-06-10 08:51:19 -0400

    Initial Commit

---
+++ NutrientCalculatorV3/NutrientCalculatorV3App.swift
@@ -0,0 +1,17 @@
+//
+//  NutrientCalculatorV3App.swift
+//  NutrientCalculatorV3
+//
+//  Created by Kyle Blandford on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct NutrientCalculatorV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
